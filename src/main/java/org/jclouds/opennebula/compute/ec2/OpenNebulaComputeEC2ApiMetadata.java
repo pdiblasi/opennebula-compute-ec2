5e52df799dda0e5ad081e4f282d09db1a111c2cd
pdiblasi <paolodiblasi86@tiscali.it>
2016-01-18 15:05:38 +0100

    new

--- src/main/java/org/jclouds/opennebula/compute/ec2/OpenNebulaComputeEC2ApiMetadata.java
+++ src/main/java/org/jclouds/opennebula/compute/ec2/OpenNebulaComputeEC2ApiMetadata.java
@@ -18,6 +18,7 @@ package org.jclouds.opennebula.compute.ec2;
 
 import java.net.URI;
 import java.util.Properties;
+import java.util.Set;
 
 import org.jclouds.apis.ApiMetadata;
 
@@ -28,6 +29,14 @@ import org.jclouds.ec2.compute.config.EC2ResolveImagesModule;
 import org.jclouds.ec2.compute.config.EC2ComputeServiceContextModule;
 import org.jclouds.ec2.config.EC2HttpApiModule;
 import org.jclouds.rest.internal.BaseHttpApiMetadata;
+
+import static org.jclouds.Constants.PROPERTY_ISO3166_CODES;
+import static org.jclouds.location.reference.LocationConstants.ISO3166_CODES;
+import static org.jclouds.location.reference.LocationConstants.PROPERTY_REGION;
+import static org.jclouds.location.reference.LocationConstants.PROPERTY_REGIONS;
+
+import com.google.common.base.Joiner;
+//import com.google.common.collect.ImmutableSet;
 
 import com.google.auto.service.AutoService;
 import com.google.common.collect.ImmutableSet;
@@ -50,8 +59,36 @@ public final class OpenNebulaComputeEC2ApiMetadata extends BaseHttpApiMetadata<E
       super(builder);
    }
 
+   public static final String EU_WEST_1 = "eu-west-1";
+   public static final String EU_CENTRAL_1 = "eu-central-1";
+   public static final String US_STANDARD = "us-standard";
+   public static final String US_EAST_1 = "us-east-1";
+   public static final String US_WEST_1 = "us-west-1";
+   public static final String US_WEST_2 = "us-west-2";
+   public static final String SA_EAST_1 = "sa-east-1";
+   public static final String AP_SOUTHEAST_2 = "ap-southeast-2";
+   public static final String AP_SOUTHEAST_1 = "ap-southeast-1";
+   public static final String AP_NORTHEAST_1 = "ap-northeast-1";
+
+   
+   public static final Set<String> DEFAULT_REGIONS = ImmutableSet.of(US_EAST_1, US_WEST_1, US_WEST_2, SA_EAST_1,
+         EU_WEST_1, EU_CENTRAL_1, AP_SOUTHEAST_1, AP_SOUTHEAST_2, AP_NORTHEAST_1);
+   
    public static Properties defaultProperties() {
       Properties properties = EC2ApiMetadata.defaultProperties();
+      
+      properties.setProperty(PROPERTY_REGIONS, Joiner.on(',').join(DEFAULT_REGIONS));
+      properties.setProperty(PROPERTY_ISO3166_CODES, "US-VA,US-CA,US-OR,BR-SP,IE,DE-HE,SG,AU-NSW,JP-13");
+      properties.setProperty(PROPERTY_REGION + "." + US_EAST_1 + "." + ISO3166_CODES, "US-VA");
+      properties.setProperty(PROPERTY_REGION + "." + US_WEST_1 + "." + ISO3166_CODES, "US-CA");
+      properties.setProperty(PROPERTY_REGION + "." + US_WEST_2 + "." + ISO3166_CODES, "US-OR");
+      properties.setProperty(PROPERTY_REGION + "." + SA_EAST_1 + "." + ISO3166_CODES, "BR-SP");
+      properties.setProperty(PROPERTY_REGION + "." + EU_WEST_1 + "." + ISO3166_CODES, "IE");
+      properties.setProperty(PROPERTY_REGION + "." + EU_CENTRAL_1 + "." + ISO3166_CODES, "DE-HE");
+      properties.setProperty(PROPERTY_REGION + "." + AP_SOUTHEAST_1 + "." + ISO3166_CODES, "SG");
+      properties.setProperty(PROPERTY_REGION + "." + AP_SOUTHEAST_2 + "." + ISO3166_CODES, "AU-NSW");
+      properties.setProperty(PROPERTY_REGION + "." + AP_NORTHEAST_1 + "." + ISO3166_CODES, "JP-13");
+      
       return properties;
    }
 
